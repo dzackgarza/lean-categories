6f1290d668196258f124b6dfb375a22fee7cc1a6
Zack Garza <dzackgarza@gmail.com>
2026-08-14 20:11:24 +0800

    feat: route standard higher categorical cells

--- LeanCategories/CategoryTheory/StandardConstructions.lean
+++ LeanCategories/CategoryTheory/StandardConstructions.lean
@@ -13,6 +13,10 @@ elements, the Grothendieck construction, fibers, and fibred categories.
 
 public import Mathlib.CategoryTheory.Elements
 public import Mathlib.CategoryTheory.Grothendieck
+public import Mathlib.CategoryTheory.Bicategory.Functor.Pseudofunctor
+public import Mathlib.CategoryTheory.Bicategory.FunctorBicategory.Pseudo
+public import Mathlib.CategoryTheory.Bicategory.Modification.Pseudo
+public import Mathlib.CategoryTheory.Bicategory.NaturalTransformation.Pseudo
 public import Mathlib.CategoryTheory.FiberedCategory.Fiber
 public import Mathlib.CategoryTheory.FiberedCategory.Fibered
 public import Mathlib.CategoryTheory.FiberedCategory.Grothendieck
